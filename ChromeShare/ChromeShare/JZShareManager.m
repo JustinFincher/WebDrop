d5dd0f549b2142eeca7bb497f765f4097665591c
Justin Fincher <zhtsu47@me.com>
2016-06-23 02:24:40 +0800

    .....

--- ChromeShare/ChromeShare/JZShareManager.m
+++ ChromeShare/ChromeShare/JZShareManager.m
@@ -58,6 +58,14 @@ static NSString *const MASOpenMenuShortcutKey = @"openMenuShortcutKey";
     shareSafariMenuItem.target = self;
     [statusBarMenu addItem:shareSafariMenuItem];
     
+    NSMenuItem *shareWebKitMenuItem = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"Share Current WebKit Tab", nil) action:@selector(shareWebKitTab) keyEquivalent:@"w"];
+    shareWebKitMenuItem.target = self;
+    [statusBarMenu addItem:shareWebKitMenuItem];
+    
+    NSMenuItem *shareSafariTechnologyPreviewMenuItem = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"Share Current Safari Technology Preview Tab", nil) action:@selector(shareSafariTechnologyPreviewTab) keyEquivalent:@"p"];
+    shareSafariTechnologyPreviewMenuItem.target = self;
+    [statusBarMenu addItem:shareSafariTechnologyPreviewMenuItem];
+    
     NSMenuItem *shareOperaMenuItem = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"Share Current Vivaldi Tab", nil) action:@selector(shareVivaldiTab) keyEquivalent:@"v"];
     shareOperaMenuItem.target = self;
     [statusBarMenu addItem:shareOperaMenuItem];
@@ -107,95 +115,42 @@ static NSString *const MASOpenMenuShortcutKey = @"openMenuShortcutKey";
 
 - (void)shareChromeTab
 {
-    [self getURLWithScript:@"chromeURL"];
+    [self getURLWithScript:@"tell application \"Google Chrome\" to get URL of active tab of front window as string"];
 }
 - (void)shareChromeCanaryTab
 {
-    [self getURLWithScript:@"chromeCanaryURL"];
+    [self getURLWithScript:@"tell application \"Google Chrome Canary\" to get URL of active tab of front window as string"];
 }
 - (void)shareSafariTab
 {
-    [self getURLWithScript:@"safariURL"];
+    [self getURLWithScript:@"tell application \"Safari\" to return URL of front document as string"];
+}
+- (void)shareSafariTechnologyPreviewTab
+{
+    [self getURLWithScript:@"tell application \"Safari Technology Preview\" to return URL of front document as string"];
 }
 - (void)shareVivaldiTab
 {
-    [self getURLWithScript:@"vivaldiURL"];
+    [self getURLWithScript:@"tell application \"Vivaldi\" to get URL of active tab of front window as string"];
 }
-
-- (NSUserAppleScriptTask *)automationScriptTask
+- (void)shareWebKitTab
 {
-    NSUserAppleScriptTask *result = nil;
-    
-    NSError *error;
-    NSURL *directoryURL = [[NSFileManager defaultManager] URLForDirectory:NSApplicationScriptsDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:&error];
-    if (directoryURL) {
-        NSURL *scriptURL = [directoryURL URLByAppendingPathComponent:@"Automation.scpt"];
-        result = [[NSUserAppleScriptTask alloc] initWithURL:scriptURL error:&error];
-        if (! result) {
-            NSLog(@"%s no AppleScript task error = %@", __PRETTY_FUNCTION__, error);
-        }
-    }
-    else {
-        // NOTE: if you're not running in a sandbox, the directory URL will always be nil
-        NSLog(@"%s no Application Scripts folder error = %@", __PRETTY_FUNCTION__, error);
-    }
-    
-    return result;
-}
-
-- (NSAppleEventDescriptor *)eventDescriptorbyName:(NSString *)string
-{
-    // target
-    ProcessSerialNumber psn = {0, kCurrentProcess};
-    NSAppleEventDescriptor *target = [NSAppleEventDescriptor descriptorWithDescriptorType:typeProcessSerialNumber bytes:&psn length:sizeof(ProcessSerialNumber)];
-    
-    // function
-    NSAppleEventDescriptor *function = [NSAppleEventDescriptor descriptorWithString:string];
-    
-    // event
-    NSAppleEventDescriptor *event = [NSAppleEventDescriptor appleEventWithEventClass:kASAppleScriptSuite eventID:kASSubroutineEvent targetDescriptor:target returnID:kAutoGenerateReturnID transactionID:kAnyTransactionID];
-    [event setParamDescriptor:function forKeyword:keyASSubroutineName];
-    
-    return event;
-}
-- (NSURL *)URLForResultEventDescriptor:(NSAppleEventDescriptor *)resultEventDescriptor
-{
-    NSURL *result = nil;
-    
-    NSString *URLString = nil;
-    if (resultEventDescriptor) {
-        if ([resultEventDescriptor descriptorType] != kAENullEvent) {
-            if ([resultEventDescriptor descriptorType] == kTXNUnicodeTextData) {
-                URLString = [resultEventDescriptor stringValue];
-            }
-        }
-    }
-    
-    result = [NSURL URLWithString:URLString];
-    
-    return result;
+    [self getURLWithScript:@"tell application \"WebKit\" to return URL of front document as string"];
 }
 
 - (void)getURLWithScript:(NSString *)scriptString
 {
-    NSUserAppleScriptTask *automationScriptTask = [self automationScriptTask];
-    if (automationScriptTask) {
-        NSAppleEventDescriptor *event = [self eventDescriptorbyName:scriptString];
-        [automationScriptTask executeWithAppleEvent:event completionHandler:^(NSAppleEventDescriptor *resultEventDescriptor, NSError *error) {
-            if (! resultEventDescriptor) {
-                NSLog(@"%s AppleScript task error = %@", __PRETTY_FUNCTION__, error);
-            }
-            else {
-                
-                
-                NSURL *URL = [self URLForResultEventDescriptor:resultEventDescriptor];
-                // NOTE: The completion handler for the script is not run on the main thread. Before you update any UI, you'll need to get
-                // on that thread by using libdispatch or performing a selector.
-                
-                
-                [self performSelectorOnMainThread:@selector(shareString:) withObject:URL waitUntilDone:NO];
-            }
-        }];
+    NSAppleScript *script= [[NSAppleScript alloc] initWithSource:scriptString];
+    NSDictionary *scriptError = nil;
+    NSAppleEventDescriptor *descriptor = [script executeAndReturnError:&scriptError];
+    if(scriptError) {
+        NSLog(@"Error: %@",scriptError);
+    } else {
+        NSAppleEventDescriptor *unicode = [descriptor coerceToDescriptorType:typeUnicodeText];
+        NSData *data = [unicode data];
+        NSString *result = [[NSString alloc] initWithCharacters:(unichar*)[data bytes] length:[data length] / sizeof(unichar)];
+        //NSLog(@"Result: %@",result);
+        [self shareString:[NSURL URLWithString:result]];
     }
 }
 -(void)shareString:(NSURL *)url
